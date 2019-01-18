d07312445c0d9af9f4003767d18004e72f58c022
Martin Vysny <martin@vysny.me>
2019-01-18 11:49:06 +0200

    minor

--- src/main/kotlin/com/github/vokorm/Utils.kt
+++ src/main/kotlin/com/github/vokorm/Utils.kt
@@ -6,7 +6,6 @@ import javax.validation.ConstraintViolation
 import javax.validation.ValidationException
 import javax.validation.Validator
 import javax.validation.executable.ExecutableValidator
-import javax.validation.metadata.BeanDescriptor
 
 fun Closeable.closeQuietly() {
     try {
