ce02ffb4921715a77ab9424d138ea45c7b43d206
Martin Vysny <mavi@vaadin.com>
2021-08-25 12:35:19 +0300

    compile to java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
 java {
     withJavadocJar()
     withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<Javadoc> {
