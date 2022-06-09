40a8cef9997799b4cab1210468eb9be37f3fdfa5
Martin Vysny <mavi@vaadin.com>
2022-06-09 08:56:32 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     api(kotlin("stdlib-jdk8"))
-    api("com.github.mvysny.vokdataloader:vok-dataloader:0.8")
+    api("com.github.mvysny.vokdataloader:vok-dataloader:0.9.1")
     api("com.gitlab.mvysny.jdbiorm:jdbi-orm:0.8")
 
     // logging
