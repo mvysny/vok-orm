3a10c2e1361581e76ece6e206d484a503223f9bd
Martin Vysny <mavi@vaadin.com>
2022-06-15 14:00:17 +0300

    more tests for filters

--- src/test/kotlin/com/github/vokorm/FiltersTest.kt
+++ src/test/kotlin/com/github/vokorm/FiltersTest.kt
@@ -34,19 +34,74 @@ class FiltersTest : DynaTest({
         }
 
         group("filter test") {
-            test("not filter test") {
+            beforeEach {
+                // create a basic set of entities
                 Person(name = "Moby", age = 25).create()
                 Person(name = "Jerry", age = 26).create()
                 Person(name = "Paul", age = 27).create()
+            }
+
+            test("eq filter test") {
+                expectList() {
+                    Person.findAllBy { Person::age eq 40 }.map { it.name }
+                }
+                expectList("Jerry") {
+                    Person.findAllBy { Person::age eq 26 }.map { it.name }
+                }
+            }
+
+            test("ne filter test") {
+                expectList("Moby", "Jerry", "Paul") {
+                    Person.findAllBy { Person::age ne 40 }.map { it.name }
+                }
+                expectList("Jerry", "Paul") {
+                    Person.findAllBy { Person::age ne 25 }.map { it.name }
+                }
+            }
+
+            test("le filter test") {
+                expectList("Moby", "Jerry", "Paul") {
+                    Person.findAllBy { Person::age le 40 }.map { it.name }
+                }
+                expectList("Moby", "Jerry") {
+                    Person.findAllBy { Person::age le 26 }.map { it.name }
+                }
+            }
+
+            test("lt filter test") {
+                expectList("Moby", "Jerry", "Paul") {
+                    Person.findAllBy { Person::age lt 40 }.map { it.name }
+                }
+                expectList("Moby") {
+                    Person.findAllBy { Person::age lt 26 }.map { it.name }
+                }
+            }
+
+            test("ge filter test") {
+                expectList() {
+                    Person.findAllBy { Person::age ge 40 }.map { it.name }
+                }
+                expectList("Jerry", "Paul") {
+                    Person.findAllBy { Person::age ge 26 }.map { it.name }
+                }
+            }
+
+            test("gt filter test") {
+                expectList() {
+                    Person.findAllBy { Person::age gt 40 }.map { it.name }
+                }
+                expectList("Paul") {
+                    Person.findAllBy { Person::age gt 26 }.map { it.name }
+                }
+            }
+
+            test("not filter test") {
                 expectList("Moby", "Paul") {
                     Person.findAllBy { !(Person::age eq 26) }.map { it.name }
                 }
             }
 
             test("in filter test") {
-                Person(name = "Moby", age = 25).create()
-                Person(name = "Jerry", age = 26).create()
-                Person(name = "Paul", age = 27).create()
                 expectList("Moby", "Jerry") {
                     Person.findAllBy { Person::age `in` listOf(25, 26, 28) }.map { it.name }
                 }
