832f654edf64fd6dd5519fda74485d50de2fc13f
Martin Vysny <mavi@vaadin.com>
2022-11-14 11:09:54 +0200

    kotlin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val slf4jVersion = "2.0.0"
 val testcontainersVersion = "1.16.2"
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.21"
     `maven-publish`
     signing
 }
