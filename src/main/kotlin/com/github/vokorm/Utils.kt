0b39c7fc2b9d8b397dedbe75c6cbed7b3a14f96a
Martin Vysny <mavi@vaadin.com>
2024-01-24 18:38:35 +0200

    #18 misc

--- src/main/kotlin/com/github/vokorm/Utils.kt
+++ src/main/kotlin/com/github/vokorm/Utils.kt
@@ -1,9 +1,9 @@
 package com.github.vokorm
 
-import com.github.mvysny.vokdataloader.DataLoaderPropertyName
-import com.github.mvysny.vokdataloader.NativePropertyName
-import com.github.mvysny.vokdataloader.SortClause
-import com.gitlab.mvysny.jdbiorm.*
+import com.gitlab.mvysny.jdbiorm.Dao
+import com.gitlab.mvysny.jdbiorm.OrderBy
+import com.gitlab.mvysny.jdbiorm.Property
+import com.gitlab.mvysny.jdbiorm.TableProperty
 import com.gitlab.mvysny.jdbiorm.quirks.DatabaseVariant
 import com.gitlab.mvysny.jdbiorm.quirks.Quirks
 import org.jdbi.v3.core.Handle
@@ -21,41 +21,18 @@ public val Handle.databaseVariant: DatabaseVariant get() = DatabaseVariant.from(
 public val Handle.quirks: Quirks get() = Quirks.from(this)
 
 /**
- * Converts [SortClause] to something like "name ASC".
- */
-private fun SortClause.toSql92OrderByClause(clazz: Class<*>): String = "${getNativeColumnName(clazz)} ${if (asc) "ASC" else "DESC"}"
-private fun SortClause.getNativeColumnName(clazz: Class<*>): NativePropertyName = propertyName.toNativeColumnName(clazz)
-
-/**
- * Converts a list of [SortClause] to something like "name DESC, age ASC". If the list is empty, returns an empty string.
- */
-internal fun List<SortClause>.toSql92OrderByClause(entityClass: Class<*>): String? = when {
-    isEmpty() -> null
-    else -> joinToString { it.toSql92OrderByClause(entityClass) }
-}
-
-/**
- * Converts the data loader property name to underlying database column name. However, if there is no such
- * property then it assumes that the data loader property name is already the column name and simply returns this.
- */
-internal fun DataLoaderPropertyName.toNativeColumnName(clazz: Class<*>): NativePropertyName {
-    val property: PropertyMeta = EntityMeta.of(clazz).properties.firstOrNull { it.name.name == this } ?: return this
-    return property.dbName.unqualifiedName
-}
-
-/**
  * Converts Kotlin [KProperty1] to JDBI-ORM Expression ([TableProperty]). That allows you to construct JDBI-ORM Conditions easily:
  * ```kotlin
  * dao.findAll(Person::id.exp.eq(25))
  * ```
  * However, it's also possible to use [buildCondition] for a more Kotlin-like Condition construction.
  */
-public inline val <reified T, V> KProperty1<T, V>.exp: TableProperty<T, V> get() = toProperty(T::class.java)
+public inline val <reified T, V> KProperty1<T, V>.exp: TableProperty<T, V> get() = toExpression(T::class.java)
 
 /**
  * Converts Kotlin [KProperty1] to JDBI-ORM Expression ([TableProperty]).
  */
-public fun <T, V> KProperty1<T, V>.toProperty(receiverClass: Class<T>): TableProperty<T, V> = TableProperty.of(receiverClass, name)
+public fun <T, V> KProperty1<T, V>.toExpression(receiverClass: Class<T>): TableProperty<T, V> = TableProperty.of(receiverClass, name)
 
 /**
  * Produces [OrderBy] suitable to be passed into [Dao.findAll]
