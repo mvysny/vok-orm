257dd40ca35346a7479cd0a6873b91f019fa2835
Martin Vysny <mavi@vaadin.com>
2024-01-24 16:49:46 +0200

    #18 Initial impl of ConditionBuilder

--- src/main/kotlin/com/github/vokorm/ConditionBuilder.kt
+++ src/main/kotlin/com/github/vokorm/ConditionBuilder.kt
@@ -31,131 +31,69 @@ public class ConditionBuilder<T : Any>(public val clazz: Class<T>) {
     public infix fun <R> KProperty1<T, R?>.le(value: R): Condition = toProperty(clazz).le(value)
 
     /**
-     * Creates an [OpFilter] with [CompareOperator.lt], requesting given property value to be less-than given [value].
+     * This property value must be less-than given [value]. Calls [Expression.lt].
      */
-    @Suppress("UNCHECKED_CAST")
-    public infix fun <R> KProperty1<T, R?>.lt(value: R): Filter<T> =
-        OpFilter(name, value as Comparable<Any>, CompareOperator.lt)
+    public infix fun <R> KProperty1<T, R?>.lt(value: R): Condition = toProperty(clazz).lt(value)
 
     /**
-     * Creates an [OpFilter] with [CompareOperator.ge], requesting given property value
-     * to be greater-than or equal to given [value].
+     * This property value must be greater-than or equal to given [value]. Calls [Expression.ge].
      */
-    @Suppress("UNCHECKED_CAST")
-    public infix fun <R> KProperty1<T, R?>.ge(value: R): Filter<T> =
-        OpFilter(name, value as Comparable<Any>, CompareOperator.ge)
+    public infix fun <R> KProperty1<T, R?>.ge(value: R): Condition = toProperty(clazz).ge(value)
 
     /**
-     * Creates an [OpFilter] with [CompareOperator.gt], requesting given
-     * property value to be greater-than given [value].
+     * This property value must be greater-than given [value]. Calls [Expression.gt].
      */
-    @Suppress("UNCHECKED_CAST")
-    public infix fun <R> KProperty1<T, R?>.gt(value: R): Filter<T> =
-        OpFilter(name, value as Comparable<Any>, CompareOperator.gt)
+    public infix fun <R> KProperty1<T, R?>.gt(value: R): Condition = toProperty(clazz).gt(value)
 
     /**
-     * Creates an [OpFilter] with [CompareOperator.ne], requesting given
-     * property value to be not equal given [value].
+     * This property value must not be equal to given [value]. Calls [Expression.ne].
      */
-    @Suppress("UNCHECKED_CAST")
-    public infix fun <R> KProperty1<T, R?>.ne(value: R): Filter<T> =
-        OpFilter(name, value as Comparable<Any>, CompareOperator.ne)
+    public infix fun <R> KProperty1<T, R?>.ne(value: R): Condition = toProperty(clazz).ne(value)
 
     /**
-     * Creates an [InFilter], requesting given property value to be one of the values
-     * provided in the [value] collection.
+     * This property value must be one of the values
+     * provided in the [value] collection. Calls [Expression.in].
      */
-    @Suppress("UNCHECKED_CAST")
-    public infix fun <C : Comparable<*>, R : Collection<C>> KProperty1<T, C>.`in`(value: R): Filter<T> = InFilter(name, value)
-
-    @Deprecated("use startsWith", ReplaceWith("this.startsWith(prefix)"))
-    public infix fun KProperty1<T, String?>.like(prefix: String): Filter<T> = startsWith(prefix)
+    public infix fun <R> KProperty1<T, R>.`in`(value: Collection<R>): Condition = toProperty(clazz).`in`(value)
 
     /**
-     * A [StartsWithFilter]. It performs the 'starts-with' matching, case-sensitive.
-     *
-     * SQL: This filter tends to perform quite well on indexed columns. If you need a substring
-     * matching, then you actually need to employ full text search
-     * capabilities of your database. For example [PostgreSQL full-text search](https://www.postgresql.org/docs/9.5/static/textsearch.html).
-     *
-     * See [contains] for substring matching.
-     * @param prefix the prefix, automatically appended with `%` when the SQL query is constructed. The 'starts-with' is matched
-     * case-sensitive.
+     * The <code>LIKE</code> operator.
+     * @param pattern e.g. "%foo%"
+     * @return the condition, not null.
      */
-    public infix fun KProperty1<T, String?>.startsWith(prefix: String): Filter<T> =
-        StartsWithFilter(name, prefix, false)
-
-    @Deprecated("use istartsWith", ReplaceWith("this.istartsWith(prefix)"))
-    public infix fun KProperty1<T, String?>.ilike(prefix: String): Filter<T> =
-        istartsWith(prefix)
+    public infix fun KProperty1<T, String?>.like(pattern: String?): Condition = toProperty(clazz).like(pattern)
 
     /**
-     * A [StartsWithFilter]. It performs the 'starts-with' matching, case-insensitive.
-     *
-     * SQL: This filter tends to perform quite well on indexed columns. If you need a substring
-     * matching, then you actually need to employ full text search
-     * capabilities of your database. For example [PostgreSQL full-text search](https://www.postgresql.org/docs/9.5/static/textsearch.html).
-     *
-     * See [icontains] for substring matching.
-     * @param prefix the prefix, automatically appended with `%` when the SQL query is constructed. The 'starts-with' is matched
-     * case-sensitive.
+     * The <code>ILIKE</code> operator.
+     * @param pattern e.g. "%foo%"
+     * @return the condition, not null.
      */
-    public infix fun KProperty1<T, String?>.istartsWith(prefix: String): Filter<T> =
-        StartsWithFilter(name, prefix)
+    public infix fun KProperty1<T, String?>.likeIgnoreCase(pattern: String?): Condition = toProperty(clazz).likeIgnoreCase(pattern)
 
     /**
      * Matches only values contained in given range.
      * @param range the range
      */
-    public infix fun <R> KProperty1<T, R?>.between(range: ClosedRange<R>): Filter<T> where R : Number, R : Comparable<R> =
-        this.ge(range.start as Number) and this.le(range.endInclusive as Number)
+    public infix fun <R : Comparable<R>> KProperty1<T, R?>.between(range: ClosedRange<R>): Condition =
+        toProperty(clazz).between(range.start, range.endInclusive)
 
     /**
-     * Matches only when the property is null. Uses [IsNullFilter].
+     * Matches only when the property is null.
      */
-    public val KProperty1<T, *>.isNull: Filter<T> get() = IsNullFilter(name)
+    public val KProperty1<T, *>.isNull: Condition get() = toProperty(clazz).isNull
 
     /**
-     * Matches only when the property is not null. Uses [IsNotNullFilter].
+     * Matches only when the property is not null.
      */
-    public val KProperty1<T, *>.isNotNull: Filter<T> get() = IsNotNullFilter(name)
+    public val KProperty1<T, *>.isNotNull: Condition get() = toProperty(clazz).isNotNull
 
     /**
-     * Matches only when the property is true. Uses [EqFilter] with the value of `true`.
+     * Matches only when the property is true. See [Expression.isTrue] for more details.
      */
-    public val KProperty1<T, Boolean?>.isTrue: Filter<T> get() = EqFilter(name, true)
+    public val KProperty1<T, Boolean?>.isTrue: Condition get() = toProperty(clazz).isTrue
 
     /**
-     * Matches only when the property is false. Uses [EqFilter] with the value of `false`.
+     * Matches only when the property is false. See [Expression.isFalse] for more details.
      */
-    public val KProperty1<T, Boolean?>.isFalse: Filter<T> get() = EqFilter(name, false)
-
-    /**
-     * Allows for a native SQL query: `"age < :age_p"("age_p" to 60)`
-     */
-    public operator fun String.invoke(vararg params: Pair<String, Any?>): Filter<T> =
-        NativeSqlFilter(this, mapOf(*params))
-
-    /**
-     * [SubstringFilter] which performs the case-sensitive 'substring' matching. Usually only used for in-memory
-     * filtering since it performs quite poorly on SQL databases.
-     *
-     * *SQL WARNING:* The database performance is very poor, even on indexed columns - the database effectively performs full
-     * table scan. Instead you should use the [FullTextFilter].
-     */
-    public infix fun KProperty1<T, String?>.contains(prefix: String): Filter<T> =
-        SubstringFilter(name, prefix, false)
-
-    /**
-     * [SubstringFilter] which performs the case-insensitive 'substring' matching. Usually only used for in-memory
-     * filtering since it performs quite poorly on SQL databases.
-     *
-     * *SQL WARNING:* The database performance is very poor, even on indexed columns - the database effectively performs full
-     * table scan. Instead you should use the [FullTextFilter].
-     */
-    public infix fun KProperty1<T, String?>.icontains(prefix: String): Filter<T> =
-        SubstringFilter(name, prefix)
+    public val KProperty1<T, Boolean?>.isFalse: Condition get() = toProperty(clazz).isFalse
 }
-
-@Deprecated("Use FilterBuilder", replaceWith = ReplaceWith("FilterBuilder<T>"))
-public typealias SqlWhereBuilder<T> = FilterBuilder<T>
